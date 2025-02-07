6d99ac6345f6036e6e4616e4125ff5f373187fbb
Bryan Gonzalez <admin@admins-MacBook-Pro.local>
2025-02-07 17:47:34 -0500

    Added SECRET_KEY

--- nova/settings.py
+++ nova/settings.py
@@ -139,3 +139,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Environment Variables
+
+SECRET_KEY = os.environ('Xv9pLq2GmY3')
+
