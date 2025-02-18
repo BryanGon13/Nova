8ca2e311c5d205a882f03f59e2f5d3e405d5adb2
Bryan Gonzalez <admin@admins-MacBook-Pro.local>
2025-02-17 19:48:06 -0500

    Add information to contact page

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'contact'  # This must match the namespace in nova/urls.py
+app_name = 'contact'  # Namespace must match 'contact' in {% url 'contact:contact' %}
 
 urlpatterns = [
-    path('', views.contact_page, name='contact_page'),  # Ensure this is correct
+    path('', views.contact_page, name='contact_page'),  # Ensure 'contact_page' is the correct name
 ]
